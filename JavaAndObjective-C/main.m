75166e543ce98178b4feac56056d2b8545a53fae
dengwu.wangdw <dengwu.wangdw@alibaba-inc.com>
2014-01-15 00:58:35 +0800

    Initial Commit

---
+++ JavaAndObjective-C/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  JavaAndObjective-C
+//
+//  Created by 王登武 on 14-1-15.
+//  Copyright (c) 2014年 wangdengwu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
